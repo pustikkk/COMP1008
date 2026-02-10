8e6d7616f2236edc98aab7e09d8064618c764c97
palexey2006 <palexey2006@gmail.com>
2026-02-10 18:32:36 -0500

    changed a bit validation

--- Assignment-1/Librarymanager.java
+++ Assignment-1/Librarymanager.java
@@ -4,25 +4,77 @@ import java.util.Scanner;
 
 public class Librarymanager {
     // Storing books
+
+    public Librarymanager() {
+        addBook();
+    }
     ArrayList<Book> books = new ArrayList<>();
-    boolean exit = false;
+
     public void addBook() {
+        Book book = new Book();
+        boolean isInputValid = false;
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter the title of the book: ");
-        String title = input.nextLine();
-        System.out.println("Enter the author of the book: ");
-        String author = input.nextLine();
-        System.out.println("Enter ISBN number: ");
-        String isbn = input.nextLine();
-        Book book = new Book(title,author,isbn,true);
+
+        // validates user input, if it is wrong, then asks again
+        while(!isInputValid) {
+            System.out.println("Enter the title of the book: ");
+            String title = input.nextLine();
+            if (book.setTitle(title)) {
+                isInputValid = true;
+            }
+        }
+
+        isInputValid = false;
+        while (!isInputValid) {
+            System.out.println("Enter the author of the book: ");
+            String author = input.nextLine();
+            if(book.setAuthor(author)) {
+                isInputValid = true;
+            }
+        }
+
+        isInputValid = false;
+        while (!isInputValid) {
+            System.out.println("Enter ISBN number: ");
+            String isbn = input.nextLine();
+            if(book.setIsbn(isbn)) {
+                isInputValid = true;
+            }
+        }
+        // availability is true by default
+        book.setAvailable(true);
+
         books.add(book);
 
     }
 
     public void displayBooks() {
+        System.out.println("Available books:");
         for (Book book : books) {
-            System.out.println(book);
+            if (book.isAvailable()) {
+                System.out.println(book);
+            }
         }
+        System.out.println("Checked out books:");
+        for (Book book : books) {
+            if (!(book.isAvailable())) {
+                System.out.println(book);
+            }
+        }
+    }
+    public void displayAvailableBooks() {
+        for (Book book : books) {
+            if (book.isAvailable()) {
+                System.out.println(book);
+            }
+        }
+    }
+
+    public void checkOutBook(Book book) {
+        book.setAvailable(false);
+    }
+    public void returnBook(Book book) {
+        book.setAvailable(true);
     }
 
 }
