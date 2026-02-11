1d29222397c3bc6e08cd3a163e93d141dd69eb82
palexey2006 <palexey2006@gmail.com>
2026-02-10 19:01:28 -0500

    Done Librarymanager class and started working on main class

---
+++ Assignment1/Assignment1/Main.java
@@ -0,0 +1,5 @@
+// Added package to have multiple main classes in one repo
+package Assignment1;
+public class Main {
+
+}
--- Assignment1/Librarymanager.java
+++ Assignment1/Librarymanager.java
@@ -6,9 +6,16 @@ public class Librarymanager {
     // Storing books
 
     public Librarymanager() {
+        // filling in the library to test
+        books.add(new Book("1984", "George Orwell", "9780451524935", true));
+        books.add(new Book("To Kill a Mockingbird", "Harper Lee", "9780061120084", false));
+        books.add(new Book("The Great Gatsby", "F. Scott Fitzgerald", "9780743273565", true));
+        books.add(new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "9780590353427", true));
+        books.add(new Book("The Catcher in the Rye", "J.D. Salinger", "9780316769488", false));
         addBook();
     }
     ArrayList<Book> books = new ArrayList<>();
+    boolean wantExit = false;
 
 
     public void addBook() {
@@ -53,20 +60,26 @@ public class Librarymanager {
         System.out.println("Available books:");
         for (Book book : books) {
             if (book.isAvailable()) {
-                System.out.println(book);
+                System.out.println("Title: " + book.getTitle());
+                System.out.println("Author: " + book.getAuthor());
+                System.out.println("ISBN number: " + book.getIsbn());
             }
         }
         System.out.println("Checked out books:");
         for (Book book : books) {
             if (!(book.isAvailable())) {
-                System.out.println(book);
+                System.out.println("Title: " + book.getTitle());
+                System.out.println("Author: " + book.getAuthor());
+                System.out.println("ISBN number: " + book.getIsbn());
             }
         }
     }
     public void displayAvailableBooks() {
         for (Book book : books) {
             if (book.isAvailable()) {
-                System.out.println(book);
+                System.out.println("Title: " + book.getTitle());
+                System.out.println("Author: " + book.getAuthor());
+                System.out.println("ISBN number: " + book.getIsbn());
             }
         }
     }
@@ -78,8 +91,24 @@ public class Librarymanager {
         book.setAvailable(true);
     }
 
-    public boolean exit(boolean exit) {
-        return exit;
+    public void searchByAuthor(String author) {
+        for (Book book : books) {
+            if(book.getAuthor().equalsIgnoreCase(author)) {
+                System.out.println("Found it!");
+                System.out.println("Title: " + book.getTitle());
+                System.out.println("Author: " + book.getAuthor());
+                System.out.println("ISBN number: " + book.getIsbn());
+                if(book.isAvailable()) {
+                    System.out.println("This book is available");
+                } else {
+                    System.out.println("Unfortunately this book is unavailable right now");
+                }
+            }
+        }
+    }
+
+    public void exit() {
+        wantExit = true;
     }
 
 }
